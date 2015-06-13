1fb8f232833db97be595ee43e93d5c22c30f00b8
NielsBang <niller-niller@hotmail.com>
2015-06-13 19:54:15 +0200

    xos compile

---
+++ src/win/hw_input.h
@@ -0,0 +1,16 @@
+// EDITED FOR USE IN WINDOWS
+
+#ifndef _hw_INPUT_H_
+#define _hw_INPUT_H_
+
+void hw_init();
+
+// void hw_ROM_to_RAM(char *dest, rom char *src);
+
+char hw_read_key();
+
+char hw_keys();
+
+char hw_wait_for_key();
+
+#endif
