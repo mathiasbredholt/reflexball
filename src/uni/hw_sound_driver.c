5bcd1eed2049c010a037ea11c9d3798eb67108bf
NielsBang <niller-niller@hotmail.com>
2015-06-18 13:05:54 +0200

    Sound fix

--- src/uni/hw_sound_driver.c
+++ src/uni/hw_sound_driver.c
@@ -1,22 +1,28 @@
-void hw_sound_init() {
 #if defined(_Z8F6403)
+#include <ez8.h>
+
+void hw_sound_init() {
+
 	// Setup timer 2 (sound)
-	// TEN: 0, TPOL: 0, PRES: 0 (1), TMODE: 3 (PWM)
-	T2CTL = 0x03;
+	// TEN: 0, TPOL: 0, PRES: 7 (128), TMODE: 3 (PWM)
+	T2CTL = 0x3B;
 
 	// Begin timer at 0
 	T2H = 0;
 	T2L = 0;
 
-	// End timer at 1843 (1 kHz)
+	// End timer at 327 (440 Hz)
 	T2RH = 0x01;
 	T2RL = 0x47;
 
+	// 50% duty cycle
+	T2PWMH = 0x00;
+	T2PWML = 0xA4;
+
+	// Enable alternate function
+	PCAF |= 0x80;
+
 	// Enable timer2
 	T2CTL |= 0x80;
-
-	// 50% duty cycle
-	T2PWMH = 0xA4;
-	T2PWML = 0x00;
-#endif
 }
+#endif
