4aa7143c85b6a0fe2aecb6f23477482a35142a02
Mathias Bredholt <mathiasbredholt@gmail.com>
2015-06-24 04:24:32 +0200

    added copyright

--- src/uni/hw_flash.h
+++ src/uni/hw_flash.h
@@ -1,13 +1,5 @@
 void hw_flash_init();
 
-unsigned int UnlockFlash(void);
-
-void LockFlash(void);
-
-unsigned int EraseFlash(unsigned int pagenum);
-
 void WriteByteToFlash(int location, unsigned int value);
-
-void WriteByteToFlash1(int location, unsigned int value);
 
 unsigned int ReadByteFromFlash(int location);
